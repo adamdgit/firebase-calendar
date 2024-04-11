e1c254f4e3ab45d0138238c1b59401c0771f7576
adamdgit <adam_d@outlook.com>
2024-04-11 17:20:34 +1000

    Added navbar component with login/logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>hi</h1>
-    </main>
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <h1>hi</h1>
+      </main>
+    </>
   );
 }
