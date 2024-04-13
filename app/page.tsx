6845d75c11cfac947378e7fa470f071207975b9d
adamdgit <adam_d@outlook.com>
2024-04-13 23:47:38 +1000

    Get events from firebase, set as state

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,53 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Calendar from "./components/Calendar";
 import Navbar from "./components/Navbar";
 import UserEvents from "./components/UserEvents";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./lib/config";
 
-export type calendarEventProps = {
+export type firebaseEventObj = {
   id: string,
-  date: string,
-  time: string,
-  description: string,
-  authorEmail: string
-}
-
-// TODO: get events only for a given month
-const temp = [
-  {
-    id: "1",
-    date: "04/12/2024",
-    time: "15:00:00",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, nostrum repudiandae est at eos itaque ipsum nihil aperiam voluptate rerum suscipit laborum impedit quos labore nisi harum, voluptates optio quisquam!",
-    authorEmail: "adammdemol@gmail.com"
-  },
-  {
-    id: "2",
-    date: "04/01/2024",
-    time: "01:30:00",
-    description: "test 2",
-    authorEmail: "adammdemol@gmail.com"
-  },
-  {
-    id: "3",
-    date: "04/01/2024",
-    time: "06:30:00",
-    description: "test 3",
-    authorEmail: "adammdemol@gmail.com"    
-  }
-]
+  author: string,
+  title: string,
+  datetime: number, // milliseconds
+  description: string
+};
 
 export default function Home() {
 
-  const [eventItems, setEventItems] = useState<calendarEventProps[]>(temp);
+  const [eventItems, setEventItems] = useState<firebaseEventObj[]>([]);
   const [updateMsg, setUpdateMsg] = useState("");
   const [needsUpdate, setNeedsUpdate] = useState(false);
+
+  async function getUserEvents() {
+    try {
+      const docRef = await getDocs(query(collection(db, "events"),
+        where('author', '==', 'adammdemol@gmail.com')
+        // where('date', '<', 'next month')
+      ));
+
+      docRef.forEach(doc => {
+        // iterate over each document and add the correct data
+        const data: firebaseEventObj = {
+          id: doc.id,
+          author: doc.data().author,
+          title: doc.data().title,
+          datetime: doc.data().datetime.seconds * 1000,
+          description: doc.data().description
+        }
+        setEventItems(prev => [...prev, data]);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // on mount, get users documents from firebase
+  useEffect(() => {
+    getUserEvents();
+  },[])
 
   return (
     <>
@@ -54,12 +59,10 @@ export default function Home() {
             eventItems={eventItems}
           />
         </div>
-        <div className="calendar-wrap">
-          <Calendar 
-            eventItems={eventItems}
-            setEventItems={setEventItems}
-          />
-        </div>
+        <Calendar 
+          eventItems={eventItems}
+          setEventItems={setEventItems}
+        />
       </main>
     </>
   );
