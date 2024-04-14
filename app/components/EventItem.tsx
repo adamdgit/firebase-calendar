ad700d7aca5573458806204acd3392c1b9550fd4
adamdgit <adam_d@outlook.com>
2024-04-14 22:57:08 +1000

    Changed time svg for events

--- app/components/EventItem.tsx
+++ app/components/EventItem.tsx
@@ -9,7 +9,7 @@ type eventItemProps = {
 
 export default function EventItem({ item, setEventItems } : eventItemProps) {
   // convert UTC date from firebase to australian time
-  const formatDate = new Date(item.datetime).toLocaleDateString('en-au', { weekday: 'long', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+  const formatDate = new Date(item.datetime).toLocaleDateString('en-au', { weekday: 'short', day: '2-digit', month: 'long', hour: '2-digit', minute: '2-digit' });
 
   const [editEnabled, setEditEnabled] = useState(false)
   const [desc, setDesc] = useState(item.description)
@@ -47,7 +47,7 @@ export default function EventItem({ item, setEventItems } : eventItemProps) {
 
       <span>{item.description}</span>
       <span className="item-date">
-        <svg width={16} height={16} fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+      <svg width={20} height={20} fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>
         {formatDate}
       </span>
     </li>
