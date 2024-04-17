c98a2c035bd3b4fd9df40127c7ad1a946cf20988
adamdgit <adam_d@outlook.com>
2024-04-17 15:51:03 +1000

    Pass update state to event items, for editing or deleting

--- app/components/UserEvents.tsx
+++ app/components/UserEvents.tsx
@@ -5,25 +5,27 @@ import type { firebaseEventObj } from "../page"
 
 type lsItemsProps = {
   eventItems: firebaseEventObj[],
-  setEventItems: (args: firebaseEventObj[]) => void
+  setEventItems: (args: firebaseEventObj[]) => void,
+  setNeedsUpdate: (args: boolean) => void,
+  setMessage: (args: string) => void
 }
 
-let currentMonth: String;
-
-export default function UserEvents({ eventItems, setEventItems } : lsItemsProps) {
+export default function UserEvents({ eventItems, setEventItems, setNeedsUpdate, setMessage }
+  : lsItemsProps) {
   // get current year and months via first event date.
   // events are retrieved from firebase based on selected month only
-  eventItems.length < 1 ? null : currentMonth = new Date(eventItems[0].datetime).toLocaleString('en-us', { month: 'long', year: 'numeric' })
 
   return (
     <div className="event-items-wrap">
-      <h3>Events for {currentMonth}:</h3>
       {
         eventItems.sort((a,b) => a.datetime - b.datetime).map((item, i) => 
           <EventItem 
             key={item.id} 
             item={item} 
+            eventItems={eventItems}
             setEventItems={setEventItems} 
+            setNeedsUpdate={setNeedsUpdate}
+            setMessage={setMessage}
           />
         )
       }
