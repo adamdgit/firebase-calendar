4a65b6405c99656781470f43a55cd9936308b3ad
adamdgit <adam_d@outlook.com>
2024-04-17 15:49:09 +1000

    Replaced navbar with a header component, new signin & signout funcs

---
+++ app/components/Header.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+import { signInWithGoogle, signOut } from '../firebase/auth';
+import { useRouter } from 'next/navigation';
+import { UserRecord } from 'firebase-admin/auth';
+
+export default function Header({ userData } : { userData: UserRecord | null }) {
+
+  const router = useRouter();
+
+  const signIn = async () => {
+    const success = await signInWithGoogle();
+    if (success) {
+      router.push('/home');
+      router.refresh();
+    }
+  }
+
+  const handleSignOut = async () => {
+    const success = await signOut();
+    if (success) {
+      router.push('/');
+      router.refresh();
+    }
+  }
+  
+  return (
+    <header className='navbar'>
+      <div className='nav-wrap'>
+        <h1>Calendar Manager</h1>:
+        <div className='user-wrap'>
+          {
+            userData ? <div className='profile'>
+              <span>{userData.email}</span>
+              <img src={userData.photoURL} alt='profile picture' />
+            </div>
+            : <></>
+          }
+          {
+            !userData ? <button onClick={() => signIn()} className='btn-primary'>Sign In</button>
+            : <button onClick={() => handleSignOut()} className='btn-primary'>Sign Out</button>
+          }
+        </div>
+      </div>
+    </header>
+  )
+}
