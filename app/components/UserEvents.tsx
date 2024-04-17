9827f335e771cd98e95d402681d39c235539c96c
adamdgit <adam_d@outlook.com>
2024-04-17 19:11:57 +1000

    Moved monthMap logic to eventItem component

--- app/components/UserEvents.tsx
+++ app/components/UserEvents.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from "react";
 import EventItem from "./EventItem";
 import type { firebaseEventObj } from "./Home";
 
@@ -7,28 +8,37 @@ type lsItemsProps = {
   eventItems: firebaseEventObj[],
   setEventItems: (args: firebaseEventObj[]) => void,
   setNeedsUpdate: (args: boolean) => void,
-  setMessage: (args: string) => void
+  setMessage: (args: string) => void,
+  currMonth: number,
+  currYear: number
 }
 
-export default function UserEvents({ eventItems, setEventItems, setNeedsUpdate, setMessage }
+export default function UserEvents({ eventItems, setEventItems, setNeedsUpdate, setMessage, currMonth, currYear }
   : lsItemsProps) {
-  // get current year and months via first event date.
-  // events are retrieved from firebase based on selected month only
+
+  // currMonth returns a number, monthMap maps the number to the word
+  const [monthMap] = useState([
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ]);
 
   return (
-    <div className="event-items-wrap">
-      {
-        eventItems.sort((a,b) => a.datetime - b.datetime).map((item, i) => 
-          <EventItem 
-            key={item.id} 
-            item={item} 
-            eventItems={eventItems}
-            setEventItems={setEventItems} 
-            setNeedsUpdate={setNeedsUpdate}
-            setMessage={setMessage}
-          />
-        )
-      }
+    <div className="events">
+      <h3>{eventItems.length > 0 ? `${eventItems.length} Events for ${monthMap[currMonth]} ${currYear}`: "No events found"}</h3><br/>
+      <div className="event-items-wrap">
+        {
+          eventItems.sort((a,b) => a.datetime - b.datetime).map((item, i) => 
+            <EventItem 
+              key={item.id} 
+              item={item} 
+              eventItems={eventItems}
+              setEventItems={setEventItems} 
+              setNeedsUpdate={setNeedsUpdate}
+              setMessage={setMessage}
+            />
+          )
+        }
+      </div>
     </div>
   )
 }
