b70478f73fca92e5d5f042e6e373f65312da5963
adamdgit <adam_d@outlook.com>
2024-04-17 19:01:34 +1000

    Conditionally render Home page if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-export default function Home() {
+import React from 'react'
+import { getCurrentUser } from './firebase/firebase-admin';
+import Home from './components/Home';
 
-  return (
+export default async function page() {
+  const currentUser = await getCurrentUser();
+  
+  // if user exists, show Home component
+  if (!currentUser) return (
     <div className="not-signedin">
       <p>Please signin with google account.<br/>
       No personal data is saved, Google handles authentication.
       </p>
     </div>
+  ) 
+  
+  if (currentUser) return (
+    <Home currentUser={JSON.parse(JSON.stringify(currentUser))}/>
   )
 }
