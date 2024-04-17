931d057c548bc43d3150cefbf961ddbfc12825a9
adamdgit <adam_d@outlook.com>
2024-04-17 15:47:29 +1000

    Added functionality for editing and deleting event items

--- app/components/EventItem.tsx
+++ app/components/EventItem.tsx
@@ -1,41 +1,110 @@
 import { useState } from "react";
-import Tooltip from "./Tooltip";
 import type { firebaseEventObj } from "../page"
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
 
 type eventItemProps = {
   item: firebaseEventObj,
-  setEventItems: (args: firebaseEventObj[]) => void
+  eventItems: firebaseEventObj[], 
+  setEventItems: (args: firebaseEventObj[]) => void,
+  setNeedsUpdate: (args: boolean) => void,
+  setMessage: (args: string) => void
 }
 
-export default function EventItem({ item, setEventItems } : eventItemProps) {
+export default function EventItem({ item, eventItems, setEventItems, setNeedsUpdate, setMessage }
+  : eventItemProps) {
   // convert UTC date from firebase to australian time
   const formatDate = new Date(item.datetime).toLocaleDateString('en-au', { weekday: 'short', day: '2-digit', month: 'long', hour: '2-digit', minute: '2-digit' });
 
-  const [editEnabled, setEditEnabled] = useState(false)
-  const [desc, setDesc] = useState(item.description)
+  const [editEnabled, setEditEnabled] = useState(false);
+  const [desc, setDesc] = useState(item.description);
+  const [title, setTitle] = useState(item.title);
+  const [date, setDate] = useState(() => {
+    let tmpYear = new Date(item.datetime).getFullYear().toString();
+    let tmpMonth = new Date(item.datetime).getMonth() < 10 ? "0" + new Date(item.datetime).getMonth().toString() : new Date(item.datetime).getMonth().toString()
+    let tempDay = new Date(item.datetime).getDate().toString();
+    return tmpYear + "-" + tmpMonth + "-" + tempDay;
+  });
+  const [hour, setHour] = useState(new Date(item.datetime).getHours().toString());
+  const [mins, setMins] = useState(new Date(item.datetime).getMinutes().toString());
 
   async function deleteEventByID(id: string) {
-
+    try {
+      await deleteDoc(doc(db, "events", id));
+      // filter out deleted event if firebase success
+      const filteredEvents = eventItems.filter(a => a.id !== id);
+      setEventItems(filteredEvents);
+      // send update message
+      setNeedsUpdate(true);
+      setMessage("Event deleted");
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  async function updateEventByID(id: string, description: string) {
+  async function updateEventByID(id: string) {
+    const formatedDate = fixDateFormat(date, hour, mins);
 
+    const data = {
+      author: "adammdemol@gmail.com",
+      title: title,
+      datetime: new Date(formatedDate),
+      description: desc
+    }
+
+    try {
+      await updateDoc(doc(db, "events", id), data);
+
+      setEditEnabled(false);
+
+      const updatedEvents = eventItems.map(item => {
+        if (item.id === id) {
+          return {
+            id: item.id,
+            author: data.author,
+            title: data.title,
+            datetime: new Date(formatedDate).getTime(),
+            description: data.description
+          }
+        } else {
+          return item;
+        }
+      })
+
+      setEventItems(updatedEvents)
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // Converts selected date and time to javascript friendly format for Date() method
+  const fixDateFormat = (date: string, hours: string, mins: string) => {
+    let dateTime = date + "T" + hours + ":" + mins + ":00Z"
+    return dateTime
   };
 
   return (
-    <li className="item-wrap">
+    <li className="item-wrap" style={editEnabled ? {border: '1px solid red'} : {}}>
+      {editEnabled ? 
+      <span className="edit-event"><div className="arrow-left"></div>Editing</span> 
+      : <></>}
+
       <div className="item-text" data-id={item.id}>
-        <span className="item-title">{item.title}</span>
+
+        {editEnabled ? 
+        <input className="edit-input" type="text" name="title" defaultValue={item.title} onChange={(e) => setTitle(e.target.value)} /> 
+        : 
+        <span className="item-title">{item.title}</span>}
+        
         <div className="item-btns">
           <button onClick={() => setEditEnabled(!editEnabled)} className="btn-edit">
-            <Tooltip message={"Edit event"} name={"edit"} />
             <svg width={20} height={20} 
               xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 512 512">
               <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
           </button>
           <button className="btn-remove" onClick={() => deleteEventByID(item.id)}>
-            <Tooltip message={"Delete event"} name={"remove"} />
             <svg width={20} height={20}
               xmlns="http://www.w3.org/2000/svg"  
               viewBox="0 0 448 512">
@@ -45,11 +114,31 @@ export default function EventItem({ item, setEventItems } : eventItemProps) {
         </div>
       </div>
 
-      <span>{item.description}</span>
+      {editEnabled ? 
+      <input className="edit-input" type="text" name="description" defaultValue={item.description} onChange={(e) => setDesc(e.target.value)} /> 
+      : 
+      <span>{item.description}</span>}
+
+      {editEnabled ? 
+      <div className="edit-datetime">
+        <input className="edit-input dates" type="date" name="date" 
+          defaultValue={date}
+          onChange={(e) => setDate(e.target.value)} />
+        <input className="edit-input hour" type="text" name="hour" 
+          defaultValue={hour}
+          onChange={(e) => setHour(e.target.value)} /> 
+        <input className="edit-input min" type="text" name="mins" 
+          defaultValue={mins}
+          onChange={(e) => setMins(e.target.value)} /> 
+      </div>
+      : 
       <span className="item-date">
       <svg width={20} height={20} fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>
         {formatDate}
-      </span>
+      </span>}
+
+      {editEnabled ? <button className="btn-save" onClick={() => updateEventByID(item.id)}>Save</button> : <></>}
+
     </li>
   )
 }
