55c820659562efed2076d58276f70eeeb180e1bc
adamdgit <adam_d@outlook.com>
2024-04-19 18:28:56 +1000

    Added a day wrapper around event items, group events by day

--- app/components/UserEvents.tsx
+++ app/components/UserEvents.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EventItem from "./EventItem";
 import type { firebaseEventObj } from "./Home";
 
@@ -21,22 +21,42 @@ export default function UserEvents({ eventItems, setEventItems, setNeedsUpdate, 
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ]);
+  const [dayWrappers, setDayWrappers] = useState<string[]>();
+
+  useEffect(() => {
+    // use set to remove duplicates, use the days to group events by day
+    setDayWrappers([...new Set(eventItems.map(item => {
+      return new Date(item.datetime).getDate().toString();
+    }))]);
+  },[eventItems])
 
   return (
     <div className="events">
       <h3>{eventItems.length > 0 ? `${eventItems.length} Events for ${monthMap[currMonth]} ${currYear}`: "No events found"}</h3><br/>
       <div className="event-items-wrap">
         {
-          eventItems.sort((a,b) => a.datetime - b.datetime).map(item => 
-            <EventItem 
-              key={item.id} 
-              item={item} 
-              eventItems={eventItems}
-              setEventItems={setEventItems} 
-              setNeedsUpdate={setNeedsUpdate}
-              setMessage={setMessage}
-            />
-          )
+          dayWrappers?.map(day => {
+            return <div key={day} className="day-wrapper">
+            <span className="day-title">
+              {new Date(`${currMonth+1}-${day}-${currYear}`)
+              .toLocaleDateString("en-au", {day: 'numeric', weekday: 'long'}).toString()}
+            </span>
+            {
+              eventItems.sort((a,b) => a.datetime - b.datetime).map(item => {
+                if (day === new Date(item.datetime).getDate().toString()) {
+                  return <EventItem 
+                    key={item.id} 
+                    item={item} 
+                    eventItems={eventItems}
+                    setEventItems={setEventItems} 
+                    setNeedsUpdate={setNeedsUpdate}
+                    setMessage={setMessage}
+                  /> 
+                }} 
+              )
+            }
+          </div>
+          })
         }
       </div>
     </div>
