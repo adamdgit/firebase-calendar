ceb830b3865e6e6dc626c2d58177beeeab58e107
adamdgit <adam_d@outlook.com>
2024-04-22 17:29:00 +1000

    Testing cookies

--- app/api/signin/route.tsx
+++ app/api/signin/route.tsx
@@ -7,15 +7,17 @@ export async function POST(request: NextRequest) {
   const reqBody = await request.json() as { idToken: string };
   const idToken = reqBody.idToken;
 
-  if (idToken) {
-    return NextResponse.json({ "success": true, "data": "Token exists" });
-  }
-
   const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
 
   const sessionCookie = await createSessionCookie(idToken, { expiresIn });
 
-  cookies().set("__session", sessionCookie, { maxAge: expiresIn, httpOnly: true, secure: true });
+  if (sessionCookie) {
+    return NextResponse.json({ success: true, data: "Cookie set", cookie: sessionCookie });
+  } else {
+    return NextResponse.json({ success: true, data: "Cookie Failed", cookie: sessionCookie });
+  }
 
-  return NextResponse.json({ "success": true, "data": "Signed in successfully." });
+  // cookies().set("__session", sessionCookie, { maxAge: expiresIn, httpOnly: true, secure: true });
+
+  // return NextResponse.json({ "success": true, "data": "Signed in successfully." });
 }
